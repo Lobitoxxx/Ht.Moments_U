2eba6ce39b75072a4dc981a1ab443d86c427d567
Lobitoxxx <luis62david@gmail.com>
2023-06-12 10:01:08 -0500

    Hotel momentos3 Final

---
+++ Ht.Momento/src/test/java/com/trabajo/Ht/Momento/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabajo.Ht.Momento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
